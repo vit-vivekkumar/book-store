f65e307d8f4519ba44e3a57662dbabe3d9eb3e19
Vivek Kumar <vivek964800@gmail.com>
2025-02-27 17:26:20 +0530

    update author

--- src/app/author-list/author-list.component.ts
+++ src/app/author-list/author-list.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-author-list',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './author-list.component.html',
   styleUrl: './author-list.component.css'
 })
-export class AuthorListComponent {
 
+
+export class AuthorListComponent implements OnInit {
+  books: any[] = [];
+
+  constructor(private authorService: AuthorService) { }
+
+  ngOnInit(): void {
+    this.loadAuthor();
+  }
+
+  loadBooks() {
+    this.authorService.getBooks().subscribe((data: any) => {
+      this.books = data;
+      console.log(this.books);
+    });
+  }
+
+  deleteBook(id: number) {
+    this.authorService.deleteBook(id).subscribe(() => {
+      this.loadBooks();
+    });
+  }
 }
